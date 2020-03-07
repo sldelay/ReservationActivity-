164b166c0c3cddbbf73ac4640d99905e67febe89
Brendan Gallagher <bpgallagher27@gmail.com>
2020-03-07 11:05:08 -0500

    server set up

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+// Dependencies
 let express = require("express");
 let path = require("path");
+
+// Sets up the Express app
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Reserved tables data
+let reservations = [];
+
+// Waitlist data
+let waitListed = [];
